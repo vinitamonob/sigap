d994ab4555f85ca03fc07e48f955f541563a1e56
magangvinita <farianivinita123@gmail.com>
2025-01-29 19:23:02 +0700

    rev: resource surat ket lain

--- app/Filament/Resources/KeteranganLainResource.php
+++ app/Filament/Resources/KeteranganLainResource.php
@@ -2,16 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\KeteranganLainResource\Pages;
-use App\Filament\Resources\KeteranganLainResource\RelationManagers;
-use App\Models\KeteranganLain;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use App\Models\KeteranganLain;
+use Filament\Resources\Resource;
+use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Fieldset;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\KeteranganLainResource\Pages;
+use Saade\FilamentAutograph\Forms\Components\SignaturePad;
+use App\Filament\Resources\KeteranganLainResource\RelationManagers;
 
 class KeteranganLainResource extends Resource
 {
@@ -25,7 +28,70 @@ class KeteranganLainResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Fieldset::make('Label')
+                    ->schema([
+                        Forms\Components\TextInput::make('nomor_surat')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('nama_ketua')
+                            ->required()
+                            ->default(Auth::user()->name)
+                            ->readOnly()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('ketua_lingkungan')
+                            ->required()
+                            ->label('Ketua Lingkungan / Stasi')
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('paroki')
+                            ->required()
+                            ->default('St. Stephanus Cilacap')
+                            ->readOnly()
+                            ->maxLength(255),
+                        Forms\Components\DatePicker::make('tanggal_surat')
+                            ->required()
+                            ->default(now())
+                            ->readOnly(),
+                        // Forms\Components\TextInput::make('tanda_tangan_pastor')
+                        //     ->maxLength(255),
+                        // Forms\Components\TextInput::make('tanda_tangan_ketua')
+                        //     ->required()
+                        //     ->maxLength(255),
+                        SignaturePad::make('tanda_tangan_ketua')
+                    ]),
+                    Fieldset::make('Data Keperluan')
+                        ->schema([
+                            Forms\Components\TextInput::make('nama_lengkap')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('tempat_lahir')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\DatePicker::make('tanggal_lahir')
+                                ->required(),
+                            Forms\Components\TextInput::make('jabatan_pekerjaan')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\Textarea::make('alamat')
+                                ->required()
+                                ->columnSpanFull(),
+                            Forms\Components\TextInput::make('telepon_rumah')
+                                ->tel()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('telepon_kantor')
+                                ->tel()
+                                ->maxLength(255),
+                            Forms\Components\Select::make('status_tinggal')
+                                ->required()
+                                ->options([
+                                    'sendiri' => 'sendiri',
+                                    'bersama keluarga' => 'bersama keluarga',
+                                    'bersama saudara' => 'bersama saudara',
+                                    'kos/kontrak' => 'kos/kontrak',
+                                ]),
+                            Forms\Components\Textarea::make('keperluan')
+                                ->required()
+                                ->columnSpanFull(),
+                        ])
             ]);
     }
 
@@ -33,7 +99,44 @@ class KeteranganLainResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('nomor_surat')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nama_ketua')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('ketua_lingkungan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('paroki')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nama_lengkap')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('tempat_lahir')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('tanggal_lahir')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('jabatan_pekerjaan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('telepon_rumah')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('telepon_kantor')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status_tinggal'),
+                Tables\Columns\TextColumn::make('tanggal_surat')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('tanda_tangan_ketua')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status_ttd_pastor')
+                    ->label('Tanda tangan pastor')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
