19ea8d420e5a3f6bb5d0f30534609c0e35e33191
magangvinita <farianivinita123@gmail.com>
2025-07-05 20:06:25 +0700

    rev

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name', 100);
             $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60);
             $table->string('tempat_lahir', 50)->nullable();
             $table->date('tgl_lahir')->nullable();
             $table->enum('jenis_kelamin', ['Pria', 'Wanita'])->nullable();
